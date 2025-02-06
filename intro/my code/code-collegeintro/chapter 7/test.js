6286b5ff043b03b10ddd90b89586630218cc2c6d
Pfanomus <221942815@edu.vut.ac.za>
2025-02-06 15:12:49 +0200

    functions and hangman

---
+++ intro/my code/code-collegeintro/chapter 7/test.js
@@ -0,0 +1,10 @@
+var colour = prompt("what is your favourite colour?");
+var js = confirm ("do you like javascript");
+console.log(colour + "colour is a nice colour.");
+if (js){
+    alert("that is cool.")
+    
+}  else {
+   alert("ya.")
+};
+
---
+++ intro/my code/code-collegeintro/chapter 8/8main.js
@@ -0,0 +1,5 @@
+var sayHello = function (myName) {
+    console.log("hello" + myName + " welcome lets code")
+};
+sayHello(' PFANO');
+sayHello(" my girl")
