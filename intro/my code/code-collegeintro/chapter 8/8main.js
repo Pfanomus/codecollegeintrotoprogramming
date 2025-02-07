548cd2236e82c8af6b89f60893e096e86dfb72d6
Pfanomus <221942815@edu.vut.ac.za>
2025-02-07 16:10:04 +0200

    chapter 5-7

--- intro/my code/code-collegeintro/chapter 8/8main.js
+++ intro/my code/code-collegeintro/chapter 8/8main.js
@@ -2,4 +2,4 @@ var sayHello = function (myName) {
     console.log("hello" + myName + " welcome lets code")
 };
 sayHello(' PFANO');
-sayHello(" my girl")
+sayHello(" girl")
---
+++ intro/my code/code-collegeintro/dummies/chapter 7/robots.js
@@ -0,0 +1,5 @@
+
+document.getElementById("lefteye").style.backgroundColor = "purple";
+document.getElementById("head").style.transform = "rotate(-15deg)";
+document.getElementById("mouth").style.backgroundColor = "pink";
+document.getElementById("rightarm").style.left = "155px";
