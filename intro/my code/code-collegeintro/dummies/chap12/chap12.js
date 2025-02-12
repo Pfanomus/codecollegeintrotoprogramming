f4eedff32175d355a5af897f314f1e36c4b05af3
Pfanomus <221942815@edu.vut.ac.za>
2025-02-12 08:28:26 +0200

    chap 11 and 12

---
+++ intro/my code/code-collegeintro/dummies/chap12/chap12.js
@@ -0,0 +1,47 @@
+var trainSpeed = 250;
+var trainPosition = 0;
+var animation;
+
+var train = document.getElementById("train");
+train.addEventListener("click", speedUp);
+
+var stopButton = document.getElementById("stopButton");
+stopButton.addEventListener("click", stopTrain);
+
+function speedUp() {
+    if (trainSpeed > 10) {
+        trainSpeed -= 10;
+    }
+    console.log("train speed: " + trainSpeed);
+
+    clearInterval(animation);
+    animation = setInterval(frame, trainSpeed);
+
+
+    function frame() {
+        trainPosition += 2;
+        if (trainPosition === 400){
+            clearInterval(animation)
+        }
+        else{
+            train.style.left = trainPosition + 'px'
+            console.log(trainPosition);
+            checkPosition(trainPosition);
+        }
+    }
+}
+
+function checkPosition(currentPosition) {
+    if (currentPosition === 400) {
+        alert("OOOOO!");
+        console.log("Crash!");
+        clearInterval(animation);
+    }
+}
+
+function stopTrain() {
+    if (trainPosition < 400) {
+        clearInterval(animation);
+    }
+}
+
---
+++ intro/my code/code-collegeintro/dummies/chapter 11/chap11.js
@@ -0,0 +1,17 @@
+var parent =["father", 'mother'];
+var myPerson = ["david",30,true];
+// var family =parent.concat(,myPerson);
+
+var myPurse ="chanel";
+var myHat = "Gucci";
+var gets =[myPurse, myHat];
+
+var array =[1, 2, 3];
+array[1]= "two";
+myPerson[0]="pfano";
+var family =parent.concat(myPerson);
+
+let longArray = [1 ,2, 3, 4, 5, 6 ,7 ,8 ,9];
+let threeOnwards =longArray.slice(2);
+let twoToSix = longArray.slice(1, 6);
+longArray.unshift(0)
